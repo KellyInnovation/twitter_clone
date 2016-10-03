5c7eb0ca10b34733cc4fdfdf6513b462712a2d2b
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-03 10:53:21 -0400

    New Setting files.

--- twitter_clone/settings_base.py
+++ twitter_clone/settings_base.py
@@ -24,10 +24,17 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+DATABASES = {}
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -79,12 +86,6 @@ WSGI_APPLICATION = 'twitter_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization
@@ -109,3 +110,5 @@ LOGOUT_URL = reverse_lazy('accounts:logout')
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
